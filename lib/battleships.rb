dc9c25c9c99cff9915deeed20f6eb29f932802d4
alexfakhri <alexfakhri@me.com>
2014-09-30 18:08:51 +0100

    Small changes made

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -7,6 +7,11 @@ class BattleShips < Sinatra::Base
     erb :index
   end
 
+  get '/newgame' do
+  	erb :newgame
+  end
+
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
